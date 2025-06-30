1b38c7ddfbc6be343d0ff6e79fa53f7be8466f23
andrea <quattrocchiandrea02@gmail.com>
2025-06-30 21:17:14 +0200

    feat: aggiunto calcolo anno pianeti

--- application.rb
+++ application.rb
@@ -1,16 +1,21 @@
 class Planet
 
-
   @radius
   @weight
   @habitable
   @area
+  @orbit_radius
+  @rotation_speed
+  @rotation_speed_trasp
+  @total_days
 
-  def initialize(r, w, h)
-    @radius = r
-    @weight = w
-    @habitable = h
-    @area = (4/3)*@radius**3*3.14
+  def initialize(r, w, h, o, v)
+    @radius = r #raggio pianeta
+    @weight = w #Peso del pianeta
+    @habitable = h #Il pianeta è abitabile?
+    @area = (4/3)*@radius**3*3.14 #Area del pianeta
+    @orbit_radius = o #Raggio dell'orbita
+    @rotation_speed = v #Velocità di rotazione nell'orbita
   end
 
   def area
@@ -26,17 +31,27 @@ class Planet
     end
   end
 
+  def duration
+    circunference = (2*3.14)*@orbit_radius
+    @rotation_speed_trasp = @rotation_speed*24
+    @total_days = circunference/@rotation_speed_trasp
+
+    print @total_days
+    print "\n"
+  end
+
 end
 
 
-giove = Planet.new(1000, 5900000, true)
-venere = Planet.new(500, 2, false)
+giove = Planet.new(71492, 80000000, true, 778000000, 47000)
+venere = Planet.new(500, 2, false, 860, 2)
 
 giove.area
 venere.area
 giove.is_habitable
 venere.is_habitable
-
+giove.duration
+venere.duration
 
 # TODO aggiungere parametro raggio orbita e velocità di rotazione, crea funzione che mi stampa la durata di un anno
 # pseudocodice
@@ -44,3 +59,7 @@ venere.is_habitable
 #calcolo circonferenza 
 #converto km/h in km/24h
 #divido la circonferenza per i km fatti in un giorno e ottengo il numero di giorni
+#
+#
+#DOMANDE:
+#
