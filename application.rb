a1bfbea6bdabf2c12367aa396f2a43a2364e45ca
Patrick Veca <patrick.veca.3@gmail.com>
2025-07-07 21:06:40 +0200

    feat: reformatted code

--- application.rb
+++ application.rb
@@ -1,6 +1,14 @@
 require_relative "src/model/Planet.rb"
 
-giove = Planet.new(radius: 71492, weight: 932876947351, habitable: true, orbit_radius: 778000000, rotation_speed: 47000)
+# L'indentazione e i caratteri di a capo sono fondamentali per avere codice leggibile.
+# Quando gli argomenti di un metodo diventano tanti è necessario riorganizzare il codice come segue:
+giove = Planet.new(radius: 71492,
+                   weight: 932876947351,
+                   habitable: true,
+                   orbit_radius: 778000000,
+                   rotation_speed: 47000)
+
+# TODO: fare anche qua
 venere = Planet.new(radius: 500, weight: 2, habitable: false, orbit_radius: 860, rotation_speed: 2)
 
 giove.area
