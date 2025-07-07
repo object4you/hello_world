b64140d9642176e8fa0347309ddaeb163f62cb81
andrea <quattrocchiandrea02@gmail.com>
2025-07-07 21:49:56 +0200

    feat: Aggiunti commenti

--- src/model/Star.rb
+++ src/model/Star.rb
@@ -1,10 +1,16 @@
+# Andiamo a caricare il file Planet.rb
 require_relative "Planet.rb"
 
+# Diachiarata la classe Star che estende la classe Planet
 class Star < Planet
+  # Dichiarata la variabile Brightness tramite l'attr_accessor
   attr_accessor :brightness
 
+  # Dichiariamo un initializer custom.
+  # @params parametri della classe pianeta più brightness
   def initialize(brightness:, radius:, weight:, habitable:, orbit_radius:, rotation_speed:)
     self.brightness = brightness
+    # Una volta inizializzati i parametri specifici di Star deleghiamo l'initializer di Planet per le altre variabili
     super(radius:, weight:, habitable:, orbit_radius:, rotation_speed:)
   end
 end
