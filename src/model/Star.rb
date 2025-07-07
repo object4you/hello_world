0220cd55b6345b61f96bcaa274ca35ca9b1e4ab3
andrea <quattrocchiandrea02@gmail.com>
2025-07-07 21:37:43 +0200

    feat: dichiarato initialize

--- src/model/Star.rb
+++ src/model/Star.rb
@@ -1,5 +1,10 @@
 require_relative "Planet.rb"
 
 class Star < Planet
-  
+  attr_accessor :brightness
+
+  def initialize(brightness:, radius:, weight:, habitable:, orbit_radius:, rotation_speed:)
+    self.brightness = brightness
+    super(radius:, weight:, habitable:, orbit_radius:, rotation_speed:)
+  end
 end
